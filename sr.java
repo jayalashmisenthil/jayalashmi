cc55ebace36eb8cefbf07715c56d5e72cab388ec
jayalashmisenthil <jayasenthil0307@gmail.com>
2016-08-30 15:56:32 +0530

    Add files via upload

---
+++ sr.java
@@ -0,0 +1,11 @@
+public class reversestring {
+public static void main(String[] args) {
+	int num;
+	String s;
+	Scanner sc=new Scanner(System.in);
+	s=sc.next();
+	sc.close();
+	num=Integer.parseInt(s);
+	System.out.println(num);
+}
+}
